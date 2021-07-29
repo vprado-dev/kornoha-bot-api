56c31848841ec3cf2832a5b13bdc8c8760e13785
Vinicius Prado <vinicavagna@gmail.com>
2021-07-29 14:19:48 -0300

    :wrench: added gif picture

--- src/endpoint/cat.ts
+++ src/endpoint/cat.ts
@@ -2,7 +2,15 @@ import { endpoint } from '@ev-fns/endpoint';
 import fetch from 'node-fetch';
 
 export const catGetOne = endpoint(async (req, res) => {
+  const flagGif = Math.floor(Math.random() * 2);
+
   const url = new URL('https://api.thecatapi.com/v1/images/search');
+
+  // eslint-disable-next-line no-extra-boolean-cast
+  if (!!flagGif) {
+    url.searchParams.append('mime_types', 'gif');
+    console.log(url.toString());
+  }
 
   const response = await fetch(url.toString(), {
     headers: {
@@ -10,7 +18,7 @@ export const catGetOne = endpoint(async (req, res) => {
     },
   });
 
-  const json = await response.json();
+  const jsonCat = await response.json();
 
-  res.status(200).json(json);
+  res.status(200).json(jsonCat);
 });
